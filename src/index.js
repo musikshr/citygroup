62098cd33305b4fb9600dbf791cde657cf5367f9
musikshr <musikshr@mail.ru>
2025-04-13 00:46:15 +0300

    four

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
+// import { BrowserRouter } from 'react-router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <BrowserRouter basename='/citygroup'> */}
+      <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
